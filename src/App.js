aab0621bb7354218a64d1c3d7f2680a17b682122
Susan Yun <susanhkyun@gmail.com>
2021-04-11 22:45:24 -0500

    hmm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 
 import LandingPage from "./components/LandingPage";
